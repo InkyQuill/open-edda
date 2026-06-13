2623b7549adf601147a3abc737f675d60fa471ca
Inky Quill <me@inkyquill.net>
2026-06-13 12:20:36 +0300

    fix: keep sqlite open helper out of generated code

--- store/db.go
+++ store/db.go
@@ -7,10 +7,6 @@ package store
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"strings"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type DBTX interface {
@@ -32,22 +28,4 @@ func (q *Queries) WithTx(tx *sql.Tx) *Queries {
 	return &Queries{
 		db: tx,
 	}
-}
-
-func Open(path string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", sqliteDSN(path))
-	if err != nil {
-		return nil, fmt.Errorf("open sqlite: %w", err)
-	}
-
-	return db, nil
-}
-
-func sqliteDSN(path string) string {
-	separator := "?"
-	if strings.Contains(path, "?") {
-		separator = "&"
-	}
-
-	return path + separator + "_foreign_keys=on&_journal_mode=WAL&_busy_timeout=5000"
 }
